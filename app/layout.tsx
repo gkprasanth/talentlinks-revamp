844d3e49506f71af58fcdaff67fb5d9772342c83
Krishna Prasanth <gkpc2004@gmail.com>
2025-02-11 00:09:41 +0530

    Add Google Tag Manager integration and new favicon images

--- app/layout.tsx
+++ app/layout.tsx
@@ -150,8 +150,22 @@ export default function RootLayout({
             }),
           }}
         />
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script id="gtm-script" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-52V4C8MK');`,
+        }} />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52V4C8MK"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
